4cc98d90bca88aff6d8e269e5de4dd9a37301f7c
Wai Yar <waiyar.aung@meds-tech.com>
2019-07-05 17:40:35 +0800

    added forecast

---
+++ README.md
@@ -0,0 +1,8 @@
+# Weather bot for Discord
+Returns weather from sg gov
+    - Currently only rainfall
+### TODO:
+    - Weather Forecast
+    - Alarm system
+        - Subscriber system
+        - Database required
--- bot.js
+++ bot.js
@@ -11,28 +11,25 @@ client.on("ready", () => {
 
 client.on("message", msg => {
     if (!msg.content[0] === "@") return;
-    switch (msg.content.slice(1)) {
-        case reqWeather: {
-            let location = msg.content.slice(reqWeather.length).trim();
-
-            let time = msg.createdAt;
-            time.setHours(time.getHours() + 8); // SG time
-            time = time.toISOString().slice(0, -5);
-
-            console.log("Msg time: ", time, ", Location: ", location);
-            getWeather(msg, location, time);
-            break;
+    if (msg.content.includes(reqWeather)) {
+        let location = msg.content.slice(reqWeather.length + 1).trim();
+        if (location.length < 4) {
+            msg.reply('Invalid location');
+            return;
         }
-        case reqHelp: {
-            const embed = new RichEmbed()
-                .setTitle('Weather Bot(WIP)')
-                .setColor('#5e2be0')
-                .setDescription('Use @weather followed by a location. E.g. @weather yishun');
-            msg.channel.send(embed);
-            break;
-        }
-        default: 
+        let time = msg.createdAt;
+        time.setHours(time.getHours() + 8); // SG time
+        time = time.toISOString().slice(0, -5);
 
+        console.log("Msg time: ", time, ", Location: ", location);
+        getWeather(msg, location, time);
+        
+    } else if (msg.content.includes(reqHelp)) {
+        const embed = new RichEmbed()
+            .setTitle('Weather Bot(WIP)')
+            .setColor('#5e2be0')
+            .setDescription('Use @weather followed by a location. E.g. @weather yishun');
+        msg.channel.send(embed);
     }
 });
 
@@ -44,7 +41,6 @@ function getWeather(msg, location, time) {
         .then(data => {
             let allStations = data.metadata.stations;
             let rainData = data.items[0].readings;
-            console.log(allStations);
 
             for (let i = 0, len = allStations.length; i < len; i++) {
                 if (allStations[i].name.toLowerCase().includes(location.toLowerCase())) {
@@ -62,6 +58,20 @@ function getWeather(msg, location, time) {
             }
         })
         .catch(err => console.error(err));
+
+    fetch(`https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=${time}`)
+        .then(res => res.json())
+        .then(data => {
+            let { valid_period, forecasts } = data.items[0];
+            for (let i = 0, len = forecasts.length; i < len; i++) {
+                if (forecasts[i].area.toLowerCase().includes(location.toLowerCase())) {
+                    msg.reply(`Weather forecast from ${valid_period.start} to ${valid_period.end}
+                        \n${forecasts[i].forecast}`);
+                    console.log(valid_period.start);
+                }
+            }
+        })
+        .catch(err => console.err(err));
 }
 
 client.login(auth.token);
