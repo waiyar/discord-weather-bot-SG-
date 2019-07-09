0105ba99cc7ecba4659725eb2d65a6b3599beecb
Wai Yar <waiyar.aung@meds-tech.com>
2019-07-09 17:43:37 +0800

    reformat functions

--- commands/weather.js
+++ commands/weather.js
@@ -1,12 +1,15 @@
 const fetch = require("node-fetch");
 const { RichEmbed } = require('discord.js');
+const { getPreciseDistance } = require('geolib');
 
 const emojis = [
     { weather: "Partly Cloudy (Day)", emojiName: ":white_sun_cloud:" },
     { weather: "Partly Cloudy (Night)", emojiName: ":cloud:" },
     { weather: "Cloudy", emojiName: ":cloud:" },
+    { weather: "Light Rain", emojiName: ":white_sun_rain_cloud:" },
     { weather: "Showers", emojiName: ":cloud_rain:" },
     { weather: "Thundery Showers", emojiName: ":thunder_cloud_rain:" },
+    { weather: "Heavy Thundery Showers with Gusty Winds", emojiName: ":thunder_cloud_rain::dash:" }
 ];
 
 module.exports = {
@@ -37,10 +40,10 @@ module.exports = {
                         matchedStation = allStations[i];
                         console.log("Station Id: ", matchedStation.id);
                         loop:
-                        for (let i = 0, len = rainData.length; i < len; i++) {
-                            if (rainData[i].station_id === matchedStation.id) {
-                                console.log(location + ": ", rainData[i].value);
-                                msg.reply(`${matchedStation.name}'s Rain: ${rainData[i].value}mm`);
+                        for (let j = 0, len = rainData.length; j < len; j++) {
+                            if (rainData[j].station_id === matchedStation.id) {
+                                console.log(location + ": ", rainData[j].value);
+                                msg.reply(`${matchedStation.name}'s Rain: ${rainData[j].value}mm`);
                                 break loop;
                             }
                         }
@@ -51,24 +54,85 @@ module.exports = {
 
         fetch(`https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=${time}`)
             .then(res => res.json())
-            .then(data => {
-                let { valid_period, forecasts } = data.items[0];
-                for (let i = 0, len = forecasts.length; i < len; i++) {
-                    if (forecasts[i].area.toLowerCase().includes(location.toLowerCase())) {
-                        let { start, end } = valid_period;
-                        start = new Date(start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });    // E.g 11:30 AM
-                        end = new Date(end).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-                        const { area, forecast } = forecasts[i];
-                        const weatherEmbed = new RichEmbed()
-                            .setColor("#38e4ff")
-                            .setTitle(`${forecast} ${emojis.find(e => e.weather === forecast).emojiName}`)
-                            .setDescription(`From ${start} to ${end}`)
-                            .setFooter(area);
-                        msg.channel.send(weatherEmbed);
-                    }
-                }
-            })
+            .then(data => this.getForecast(data, location, msg))
             .catch(err => console.error(err));
+    },
+    getForecast(data, location, msg) {
+        let { valid_period, forecasts } = data.items[0];
+        for (let i = 0, len = forecasts.length; i < len; i++) {
+            if (forecasts[i].area.toLowerCase().includes(location.toLowerCase())) {
+                let { start, end } = valid_period;
+                start = new Date(start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });    // E.g 11:30 AM
+                end = new Date(end).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+                const { area, forecast } = forecasts[i];
+                const weatherEmbed = new RichEmbed()
+                    .setColor("#38e4ff")
+                    .setTitle(`${forecast} ${emojis.find(e => e.weather === forecast).emojiName}`)
+                    .setDescription(`From ${start} to ${end}`)
+                    .setFooter(area);
+                return msg.channel.send(weatherEmbed);
+            } else if (i == len - 1) { // Last of loop, did not match
+                fetch(`https://developers.onemap.sg/commonapi/search?searchVal=${location}&returnGeom=Y&getAddrDetails=N&pageNum=1`)
+                    .then(res => res.json())
+                    .then(geoData => {
+                        const { found, results } = geoData;
+                        if (found > 0) {
+                            const { LATITUDE: lat, LONGITUDE: lon } = results[0];   // Assuming that the top result is appropriately located
+                            const areas = data.area_metadata;
+                            let distance = 25000; // 25 km as limit
+
+                            for (let j = 0; j < areas.length; j++) {    // Searches the nearest zone
+                                let buff = getPreciseDistance({ lat, lon }, areas[j].label_location);
+                                if (buff < distance) {
+                                    distance = buff;
+                                    location = areas[j].name;
+                                }
+                            }
+                            return this.getForecast(data, location, msg);
+                        }
+                    })
+                    .catch(err => console.error(err));
+            }
+        }
     }
 }
+
+/* function getForecast(data, location, msg) {
+    let { valid_period, forecasts } = data.items[0];
+    for (let i = 0, len = forecasts.length; i < len; i++) {
+        if (forecasts[i].area.toLowerCase().includes(location.toLowerCase())) {
+            let { start, end } = valid_period;
+            start = new Date(start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });    // E.g 11:30 AM
+            end = new Date(end).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+            const { area, forecast } = forecasts[i];
+            const weatherEmbed = new RichEmbed()
+                .setColor("#38e4ff")
+                .setTitle(`${forecast} ${emojis.find(e => e.weather === forecast).emojiName}`)
+                .setDescription(`From ${start} to ${end}`)
+                .setFooter(area);
+            return msg.channel.send(weatherEmbed);
+        } else if (i == len - 1) { // Last of loop, did not match
+            fetch(`https://developers.onemap.sg/commonapi/search?searchVal=${location}&returnGeom=Y&getAddrDetails=N&pageNum=1`)
+                .then(res => res.json())
+                .then(geoData => {
+                    const { found, results } = geoData;
+                    if (found > 0) {
+                        const { LATITUDE: lat, LONGITUDE: lon } = results[0];   // Assuming that the top result is appropriately located
+                        const areas = data.area_metadata;
+                        let distance = 25000; // 25 km as limit
+
+                        for (let j = 0; j < areas.length; j++) {    // Searches the nearest zone
+                            let buff = getPreciseDistance({ lat, lon }, areas[j].label_location);
+                            if (buff < distance) {
+                                distance = buff;
+                                location = areas[j].name;
+                            }
+                        }
+                        return getForecast(data, location, msg);
+                    }
+                })
+        }
+    }
+} */
